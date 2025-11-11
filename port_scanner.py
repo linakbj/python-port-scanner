ed4e7a40b21e1b13356a99ad5d79ae52c28e3a7f
Lina <linakabbaj1@gmail.com>
2025-11-11 22:35:18 +0100

    feat: add CLI port scanner with optional output file

--- port_scanner.py
+++ port_scanner.py
@@ -1,19 +1,25 @@
 import socket
 import argparse
 
-COMMON_PORTS = {21 : "FTP",
+COMMON_PORTS = {20 : "FTP",
+                21 : "FTP",
                 22 : "SSH",
                 23 : "Telnet",
+                24 : "LMTP",
                 25 : "SMTP",
                 53 : "DNS",
                 80 : "HTTP",
                 88 : "Kerberos",
+                101 : "NIC",
+                107 : "RTelnet",
+                115 : "Simple File Transfer Protocol",
                 143 : "IMAP",
+                179 : "BGP",
                 443 : "HTTPS",
                 3306 : "MySQL",
                 3389 : "RDP"}
 
-def port_checker(target_ip : str, start_port : int, end_port : id, timeout : float) :
+def port_checker(target_ip : str, start_port : int, end_port : id, timeout : float, output_file : str | None) :
 
     print(f"Scanning {target_ip} from {start_port} to {end_port}")
 
@@ -23,7 +29,13 @@ def port_checker(target_ip : str, start_port : int, end_port : id, timeout : flo
         try:
             s.connect((target_ip, port))
             service = COMMON_PORTS.get(port, "unknown")
-            print(f"{port}/tcp open -> Service : {service} ")
+            line = (f"{port}/tcp open -> Service : {service} ")
+            print(line)
+
+            if output_file :
+                with open(output_file, "a") as f:
+                    f.write(line + "\n")
+
         except (socket.timeout, ConnectionRefusedError, OSError):
             pass
         finally:
@@ -38,6 +50,7 @@ def main() :
     parser.add_argument("--start", type=int, required=True, help="Beginning port")
     parser.add_argument("--end", type=int, required=True, help="End port")
     parser.add_argument("--timeout", type=float, default=1.5, help="Timeout in seconds")
+    parser.add_argument("--output", help="Write open ports to a file")
 
     args = parser.parse_args()
 
@@ -45,7 +58,7 @@ def main() :
         print("Range is invalid. Provide a range between 1 and 65535, with start <= end")
         return
 
-    port_checker(args.ip, args.start, args.end, args.timeout)
+    port_checker(args.ip, args.start, args.end, args.timeout, args.output)
 
 if __name__ == "__main__":
     main()
